ef099b5856c1c8b4cc1c0ebd54cd8381ffb71f67
Nefj <nefj@outlook.com>
2018-06-05 11:49:24 -0400

    mod excel

--- fe/src/app/events/persons-of-events/persons-of-events.component.ts
+++ fe/src/app/events/persons-of-events/persons-of-events.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PersonaService } from '../../services/persona.service';
-import { PeticionesService} from '../../services/peticiones.service';
+import { PeticionesService } from '../../services/peticiones.service';
 import { ActivatedRoute, Router } from "@angular/router";
 import { Pipe, PipeTransform } from '@angular/core';
 import { Identity } from '../../services/global';
@@ -12,20 +12,22 @@ import { Angular5Csv } from 'angular5-csv/Angular5-csv';
   selector: 'app-persons-of-events',
   templateUrl: './persons-of-events.component.html',
   styleUrls: ['./persons-of-events.component.css'],
-  providers:[PersonaService,PeticionesService]
+  providers: [PersonaService, PeticionesService]
 
 
 })
 export class PersonsOfEventsComponent implements OnInit {
 
- 
+
   public eventId;
   public event;
-  public lista_personas=[];
-  public lista_personasPorInteres=[];
+  public lista_personas = [];
+  public lista_personasPorInteres = [];
   public listaInteres;
   public persona;
   public listaReturned;
+  private listaToExport = [];
+
   // public listaToExport=[];
   public toExport;
   constructor(
@@ -33,113 +35,125 @@ export class PersonsOfEventsComponent implements OnInit {
     private route: ActivatedRoute,
     private _personaService: PersonaService,
     private _peticionesService: PeticionesService
-   
+
 
 
   ) { }
 
   ngOnInit() {
-    this.route.params.subscribe(params=>{
-        this.eventId=params.id;
-        this.loadPersons2();
-    })
-    
-
-  }
-  loadPersons2(){
-    this.lista_personasPorInteres=[];
-    this._peticionesService.getEvent(this.eventId).subscribe(response=>{
-          this.event=response;
-          this._peticionesService.getPersonasInteresWithEvent(this.event).subscribe(res=>{
-            this.listaReturned=res;
-            this.lista_personasPorInteres=this.listaReturned;
-          })
-    })
-    
-  }
- 
-  loadPersonsInteres2(numInteres){
-    this.lista_personasPorInteres=[];
-    let eventinteres={}as EventInteres;
-    eventinteres.event=this.event;
-    eventinteres.interes=numInteres;
-    this._peticionesService.getPersonFilterInteresWithEvent(eventinteres).subscribe(response=>{
-      this.listaReturned=response;
-      this.lista_personasPorInteres=this.listaReturned;
+    this.route.params.subscribe(params => {
+      this.eventId = params.id;
+      this.loadPersons2();
     })
 
 
+  }
+  loadPersons2() {
+    this.lista_personasPorInteres = [];
+    this._peticionesService.getEvent(this.eventId).subscribe(response => {
+      this.event = response;
+      this._peticionesService.getPersonasInteresWithEvent(this.event).subscribe(res => {
+        this.listaReturned = res;
+        this.lista_personasPorInteres = this.listaReturned;
+      })
+    })
 
   }
-  
-  edit(_id:string){
-    var personIdEventId=_id+'-'+this.eventId;
-    this.router.navigate(['home/events/persons/edit',personIdEventId]);
+
+  loadPersonsInteres2(numInteres) {
+    this.lista_personasPorInteres = [];
+    let eventinteres = {} as EventInteres;
+    eventinteres.event = this.event;
+    eventinteres.interes = numInteres;
+    this._peticionesService.getPersonFilterInteresWithEvent(eventinteres).subscribe(response => {
+      this.listaReturned = response;
+      this.lista_personasPorInteres = this.listaReturned;
+    })
+
+
+
+  }
+
+  edit(_id: string) {
+    var personIdEventId = _id + '-' + this.eventId;
+    this.router.navigate(['home/events/persons/edit', personIdEventId]);
 
   }
 
 
 
-  exportarExcel(){
-    let listaToExport=[];
-    let personToExport={} as PersonToExport;
-    for(let p of this.lista_personasPorInteres){
-      personToExport.first_name=p.first_name;
-      personToExport.last_name=p.last_name;
-      personToExport.city=p.city;
-      personToExport.cellphone=p.cellphone;
-      personToExport.interes=p.stateName;
-      listaToExport.push(personToExport);
-    }
-    let options = { 
+  exportarExcel() {
+    nPersons(0, this.lista_personasPorInteres, this.listaToExport, this.event.name);
+    let options = {
       fieldSeparator: ',',
       quoteStrings: '"',
       decimalseparator: '.',
-      showLabels: true, 
-      showTitle: true,
+      showLabels: false,
+      showTitle: false,
       useBom: true,
       // noDownload: true,
-      headers: ["NOMBRES", "APELLIDOS", "CIUDAD","CELULAR","INTERES"]
+      // headers: ["NOMBRES", "APELLIDOS", "CIUDAD", "CELULAR", "Móvil"]
+      headers: [
+        document.getElementById('grupo').value,
+        document.getElementById('PrimerNombre').value,
+        document.getElementById('SegundoNombre').value,
+        document.getElementById('Apellido').value,
+        document.getElementById('Celular').value,
+      ]
     };
-   
-
-    this.toExport=new Angular5Csv(listaToExport,"Nuevo Reporte",options);
+    // console.log(this.listaToExport);
+    this.toExport = new Angular5Csv(this.listaToExport, "Nuevo Reporte", options);
   }
-   
 
 
 }
-export interface EventInteres{
-  event:Object,
-  interes:number;
+function nPersons(i, lista_personasPorInteres, listaToExport, name) {
+  console.log(i, lista_personasPorInteres.length);
+  if (i == lista_personasPorInteres.length) return;
+  let personToExport = {} as PersonToExport;
+  // for (let p of lista_personasPorInteres) {
+  personToExport.grupo = name;
+  personToExport.first_name = lista_personasPorInteres[i].first_name;
+  personToExport.middle_name = '';
+  personToExport.last_name = lista_personasPorInteres[i].last_name;
+  personToExport.cellphone = lista_personasPorInteres[i].cellphone;
+  listaToExport.push(personToExport);
+  // }
+  return nPersons(i + 1, lista_personasPorInteres, listaToExport, name);
 }
-export interface EventIdPersonId{
-  eventId:string,
-  personId:string,
+// function nPerson(lista, lista_personas) {
+//   if(i)
+// }
+export interface EventInteres {
+  event: Object,
+  interes: number;
+}
+export interface EventIdPersonId {
+  eventId: string,
+  personId: string,
 }
 
-export interface EventPerson{
-  _id:string,
-  person:Object,
-  state:number,
+export interface EventPerson {
+  _id: string,
+  person: Object,
+  state: number,
 }
-export interface PersonItem{
+export interface PersonItem {
 
-  _id:string,
-  first_name:string,
-  last_name:string,
-  cellphone:number,
-  city:string,
-  state:number,
-  stateName:string,
+  _id: string,
+  first_name: string,
+  last_name: string,
+  cellphone: number,
+  city: string,
+  state: number,
+  stateName: string,
 
 
 }
-export interface PersonToExport{
-  first_name:string,
-  last_name:string,
-  cellphone:number,
-  city:string,
-  interes:string
-
+export interface PersonToExport {
+  grupo: string,
+  first_name: string,
+  middle_name: string,
+  last_name: string,
+  cellphone: number,
 }
